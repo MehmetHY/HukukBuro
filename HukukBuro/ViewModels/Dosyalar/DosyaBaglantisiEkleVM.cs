0b395156f352099aa25cd9231d6c637785cec8d9
MehmetHY <mehmethakkiyildiz@yahoo.com>
2023-05-26 20:35:32 +0300

    dosya baglantisi ekle frontend yapildi

--- HukukBuro/ViewModels/Dosyalar/DosyaBaglantisiEkleVM.cs
+++ HukukBuro/ViewModels/Dosyalar/DosyaBaglantisiEkleVM.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace HukukBuro.ViewModels.Dosyalar;
 
@@ -6,8 +7,10 @@ public class DosyaBaglantisiEkleVM
 {
     public int DosyaId { get; set; }
 
+    [Display(Name = "Dosya")]
     public int IlgiliDosyaId { get; set; }
     public List<SelectListItem> Dosyalar { get; set; } = new();
 
+    [Display(Name = "Açıklama")]
     public string? Aciklama { get; set; }
 }
