490ef1f9913cb63c0f341e7a3e6201d731419e43
MehmetHY <mehmethakkiyildiz@yahoo.com>
2023-05-26 18:41:08 +0300

    kisi belgesi duzenle frontend yapildi

--- HukukBuro/ViewModels/Kisiler/KisiBelgesiDuzenleVM.cs
+++ HukukBuro/ViewModels/Kisiler/KisiBelgesiDuzenleVM.cs
@@ -1,4 +1,8 @@
-﻿namespace HukukBuro.ViewModels.Kisiler;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HukukBuro.ViewModels.Kisiler;
+
+#pragma warning disable CS8618
 
 public class KisiBelgesiDuzenleVM
 {
@@ -6,10 +10,15 @@ public class KisiBelgesiDuzenleVM
 
     public int KisiId { get; set; }
 
+
+    [Display(Name = "Başlık")]
+    [Required(ErrorMessage = Sabit.Hata.Gerekli, AllowEmptyStrings = false)]
     public string Baslik { get; set; }
 
+    [Display(Name = "Açıklama")]
     public string? Aciklama { get; set; }
 
+    [Display(Name = "Özel mi?")]
     public bool OzelMi { get; set; }
 
     public bool BelgeyiDegistir { get; set; }
