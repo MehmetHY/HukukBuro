249afad34c68fd6ebc2b576018d759778b78309a
MehmetHY <mehmethakkiyildiz@yahoo.com>
2023-05-27 17:41:52 +0300

    kisi ozet duzenle frontend yapildi

--- HukukBuro/ViewModels/Kisiler/KisiOzetDuzenleVM.cs
+++ HukukBuro/ViewModels/Kisiler/KisiOzetDuzenleVM.cs
@@ -9,25 +9,34 @@ public class KisiOzetDuzenleVM
     [Required]
     public bool TuzelMi { get; set; }
 
+    [Display(Name = "İsim")]
     public string? Isim { get; set; }
 
     public string? Soyisim { get; set; }
 
+    [Display(Name = "T.C. Kimlik No")]
     public string? TcKimlikNo { get; set; }
 
+    [Display(Name = "Şirket İsmi")]
     public string? SirketIsmi { get; set; }
 
+    [Display(Name = "Vergi No")]
     public string? VergiNo { get; set; }
 
+    [Display(Name = "Vergi Dairesi")]
     public string? VergiDairesi { get; set; }
 
+    [Phone]
     public string? Telefon { get; set; }
 
+    [EmailAddress]
     public string? Email { get; set; }
 
     public string? Adres { get; set; }
 
+    [Display(Name = "Banka Hesap Bilgisi")]
     public string? BankaHesapBilgisi { get; set; }
 
+    [Display(Name = "Ek Bilgi")]
     public string? EkBilgi { get; set; }
 }
