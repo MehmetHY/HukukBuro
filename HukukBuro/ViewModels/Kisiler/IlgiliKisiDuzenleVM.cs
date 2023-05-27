eba98f8db3c233235ce8f6640820a966cc2eca83
MehmetHY <mehmethakkiyildiz@yahoo.com>
2023-05-27 17:18:37 +0300

    ilgili kisi duzenle frontend yapildi

--- HukukBuro/ViewModels/Kisiler/IlgiliKisiDuzenleVM.cs
+++ HukukBuro/ViewModels/Kisiler/IlgiliKisiDuzenleVM.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace HukukBuro.ViewModels.Kisiler;
 
@@ -8,6 +9,7 @@ public class IlgiliKisiDuzenleVM
 
     public int KisiId { get; set; }
 
+    [Display(Name = "Kişi")]
     public int IlgiliKisiId { get; set; }
 
     public List<SelectListItem> Kisiler { get; set; } = new();
