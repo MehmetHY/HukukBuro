ee4e54b18873ba90281c450dfefc4fcafcc163f6
MehmetHY <mehmethakkiyildiz@yahoo.com>
2023-05-27 10:36:49 +0300

    duyuru duzenle frontend yapildi

--- HukukBuro/ViewModels/Duyurular/DuzenleVM.cs
+++ HukukBuro/ViewModels/Duyurular/DuzenleVM.cs
@@ -1,4 +1,6 @@
-﻿namespace HukukBuro.ViewModels.Duyurular;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HukukBuro.ViewModels.Duyurular;
 
 #pragma warning disable CS8618
 
@@ -6,11 +8,14 @@ public class DuzenleVM
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = Sabit.Hata.Gerekli, AllowEmptyStrings = false)]
     public string Konu { get; set; }
 
+    [Required(ErrorMessage = Sabit.Hata.Gerekli, AllowEmptyStrings = false)]
     public string Mesaj { get; set; }
 
     public string? Url { get; set; }
 
+    [Display(Name = "Bildirim gönder")]
     public bool BildirimGonder { get; set; }
 }
