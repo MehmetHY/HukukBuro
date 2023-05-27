cbf7fc9d76ffac8d7f5f762bb7dabe80ca5d2493
MehmetHY <mehmethakkiyildiz@yahoo.com>
2023-05-27 13:39:16 +0300

    gorev duzenle frontend yapildi

--- HukukBuro/ViewModels/Gorevler/DuzenleVM.cs
+++ HukukBuro/ViewModels/Gorevler/DuzenleVM.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace HukukBuro.ViewModels.Gorevler;
 
@@ -8,23 +9,34 @@ public class DuzenleVM
 {
     public int Id { get; set; }
 
+    [Display(Name = "Bağlantı Türü")]
+    [Required(ErrorMessage = Sabit.Hata.Gerekli, AllowEmptyStrings = false)]
     public BaglantiTuru BaglantiTuru { get; set; }
 
+    [Display(Name = "Dosya")]
     public int? DosyaId { get; set; }
     public List<SelectListItem> Dosyalar { get; set; } = new();
 
+    [Display(Name = "Kişi")]
     public int? KisiId { get; set; }
     public List<SelectListItem> Kisiler { get; set; } = new();
 
+    [Required(ErrorMessage = Sabit.Hata.Gerekli, AllowEmptyStrings = false)]
     public string Konu { get; set; }
 
+    [Display(Name = "Açıklama")]
     public string? Aciklama { get; set; }
 
+    [Display(Name = "Bitiş Tarihi")]
+    [Required(ErrorMessage = Sabit.Hata.Gerekli, AllowEmptyStrings = false)]
     public DateTime BitisTarihi { get; set; } = DateTime.Now;
 
+    [Display(Name = "Durum")]
+    [Required(ErrorMessage = Sabit.Hata.Gerekli, AllowEmptyStrings = false)]
     public int DurumId { get; set; }
     public List<SelectListItem> Durumlar { get; set; } = new();
 
+    [Display(Name = "Sorumlu")]
     public string? SorumluId { get; set; }
     public List<SelectListItem> Personel { get; set; } = new();
 }
