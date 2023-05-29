f770251466d3d9f1fa15403e0d6f0e07103cc581
MehmetHY <mehmethakkiyildiz@yahoo.com>
2023-05-29 12:23:42 +0300

    randevu ekle frontend yapildi

--- HukukBuro/ViewModels/Randevular/EkleVM.cs
+++ HukukBuro/ViewModels/Randevular/EkleVM.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace HukukBuro.ViewModels.Randevular;
 
@@ -6,17 +7,21 @@ namespace HukukBuro.ViewModels.Randevular;
 
 public class EkleVM
 {
+    [Display(Name = "Kişi")]
     public int KisiId { get; set; }
     public List<SelectListItem> Kisiler { get; set; } = new();
 
+    [Required(ErrorMessage = Sabit.Hata.Gerekli, AllowEmptyStrings = false)]
     public string Konu { get; set; }
 
+    [Display(Name = "Açıklama")]
     public string? Aciklama { get; set; }
 
     public DateTime Tarih { get; set; } = DateTime.Now;
 
     public bool TamamlandiMi { get; set; }
 
+    [Display(Name = "Sorumlu")]
     public string? SorumluId { get; set; }
     public List<SelectListItem> Personel { get; set; } = new();
 }
