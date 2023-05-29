68ab838e3f50c0c14c5c0ce5335c5f3de3edefbc
MehmetHY <mehmethakkiyildiz@yahoo.com>
2023-05-29 12:26:34 +0300

    randevu duzenle frontend yapildi

--- HukukBuro/ViewModels/Randevular/DuzenleVM.cs
+++ HukukBuro/ViewModels/Randevular/DuzenleVM.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace HukukBuro.ViewModels.Randevular;
 
@@ -8,17 +9,21 @@ public class DuzenleVM
 {
     public int Id { get; set; }
 
+    [Display(Name = "Kişi")]
     public int KisiId { get; set; }
     public List<SelectListItem> Kisiler { get; set; } = new();
 
+    [Required(ErrorMessage = Sabit.Hata.Gerekli, AllowEmptyStrings = false)]
     public string Konu { get; set; }
 
+    [Display(Name = "Açıklama")]
     public string? Aciklama { get; set; }
 
-    public DateTime Tarih { get; set; }
+    public DateTime Tarih { get; set; } = DateTime.Now;
 
     public bool TamamlandiMi { get; set; }
 
+    [Display(Name = "Sorumlu")]
     public string? SorumluId { get; set; }
     public List<SelectListItem> Personel { get; set; } = new();
 }
